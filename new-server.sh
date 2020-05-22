7181796166e37b812afa7c0bc025b47493368b6a
McKenzie Flavius <mckenzie@McKenzies-MacBook-Pro.local>
2020-05-22 03:54:01 +0100

    Added multiple visual feedback for file

--- new-server.sh
+++ new-server.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
+cat << EOF
+Welcome!
+
+We're going to try installing all the software needed to run Wordpress on this server, please follow instructions.
+EOF
+
+cat << EOF
+  First we're going to update the server.
+EOF
 sudo apt update
 
+cat << EOF
+  Next we'll install Nginx
+EOF
 sudo apt install nginx
 
+cat << EOF
+  Now we're going to allow it access to certain ports.
+EOF
 sudo ufw allow 'Nginx HTTP'
 
+cat << EOF
+  Now were going to install MySQL
+EOF
 sudo apt install mysql-server
 
+cat << EOF
+  Now for some security updates to MySQL
+EOF
 sudo mysql_secure_installation
